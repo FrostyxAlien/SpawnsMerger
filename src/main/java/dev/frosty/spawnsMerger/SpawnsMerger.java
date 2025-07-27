3a875c34ff84175a9d969893acf80088cb1a2f05
FrostyxAlien <frostyalien@outlook.de>
2025-07-27 11:39:40 +0200

    new

---
+++ src/main/java/dev/frosty/spawnsMerger/SpawnsMerger.java
@@ -0,0 +1,17 @@
+package dev.frosty.spawnsMerger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpawnsMerger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
